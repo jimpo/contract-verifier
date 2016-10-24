30b75129a349bfab90b93e5cf951cad0211bbb3b
Jim Posen <jimpo@coinbase.com>
2016-10-23 17:17:15 -0700

    Create React component.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Application from './Application';
+
+
+const sourceLinkedABI = [{"constant":true,"inputs":[],"name":"sourceURL","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"}];
+
+window.addEventListener('load', () => {
+  const SourceLinked = web3.eth.contract(sourceLinkedABI);
+
+  ReactDOM.render(
+    <Application/>,
+    document.getElementById('root')
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,26 @@ module.exports = {
     publicPath: '/assets/',
     filename: 'app.bundle.js'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.json$/,
+        loaders: ['json']
+      },
+      {
+        test: /\.less$/,
+        loaders: ['style', 'css', 'less']
+      }
+    ]
+  },
   devServer: {
     host: '0.0.0.0',
     contentBase: './dist'
